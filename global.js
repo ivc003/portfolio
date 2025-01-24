1e564136c9d7ee1c692cd2db838d5a7b4e2eece5
ivc003 <ivc003@ucsd.edu>
2025-01-24 14:36:51 -0800

    working on improvements

---
+++ global.js
@@ -0,0 +1,50 @@
+console.log('IT’S ALIVE!!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: 'Resume'},
+    { url: 'https://github.com/ivc003', title: 'Profile'}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+      }
+
+    nav.append(a);
+  }
+
+
+// const navLinks = $$("nav a");
+// console.log(navLinks);
+// console.log([1, 2, 3, 4].find(n => n > 2));
+// console.log(location.host);
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//   );
+
+// currentLink?.classList.add('current');
+
+// console.log(currentLink);
