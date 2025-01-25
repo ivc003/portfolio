d425135eb1faae2d03d9a46aaee3f14820903551
ivc003 <ivc003@ucsd.edu>
2025-01-24 23:08:29 -0800

    hope

--- global.js
+++ global.js
@@ -6,9 +6,9 @@ function $$(selector, context = document) {
 
 let pages = [
     { url: '', title: 'Home' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact'},
-    { url: 'resume/', title: 'Resume'},
+    { url: '../projects/index.html', title: 'Projects' },
+    { url: '../contact/index.html', title: 'Contact'},
+    { url: '../resume/index.html', title: 'Resume'},
     { url: 'https://github.com/ivc003', title: 'Profile'}
   ];
 
