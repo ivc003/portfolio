5e3302ff067c2969d94be68d8408f472aa8f0bb3
ivc003 <ivc003@ucsd.edu>
2025-01-25 00:06:51 -0800

    dark purple with bar

--- global.js
+++ global.js
@@ -22,7 +22,7 @@ for (let p of pages) {
     let title = p.title;
     const ARE_WE_HOME = document.documentElement.classList.contains('home');
 
-    if (!ARE_WE_HOME && !url.startsWith('http')) {
+    if (!url.startsWith('http')) {
         url = '../../' + url;
       }
 
