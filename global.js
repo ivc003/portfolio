0822cb2c5989f4f947783a8899b9f1b19f455e09
ivc003 <ivc003@ucsd.edu>
2025-01-31 14:51:45 -0800

    working on it

--- global.js
+++ global.js
@@ -75,3 +75,33 @@ select.addEventListener('input', function (event) {
 
     console.log('New color scheme:', event.target.value);
 });
+
+export async function fetchJSON(url) {
+  try {
+      // Fetch the JSON file from the given URL
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    console.log(response)
+    const data = await response.json();
+    return data;
+
+
+  } catch (error) {
+      console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  // Your code will go here
+  containerElement.innerHTML = '';
+
+  const article = document.createElement('article');
+  article.innerHTML = `
+    <h2>${project.title}</h2>
+    <img src="${project.image}" alt="${project.title}">
+    <p>${project.description}</p>
+`;
+  containerElement.appendChild(article);
+}
