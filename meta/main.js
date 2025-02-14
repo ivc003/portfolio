354ec80603d4b5de8cff13f7b5770304ce409a5c
ivc003 <ivc003@ucsd.edu>
2025-02-13 17:40:11 -0800

    plot!

--- meta/main.js
+++ meta/main.js
@@ -1,10 +1,160 @@
 let data = [];
 
 async function loadData() {
-  data = await d3.csv('loc.csv');
-  console.log(data);
+  data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line), // or just +row.line
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+
+  processCommits();
+  console.log(commits);
+  displayStats();
+  createScatterplot()
+}
+
+let commits = d3.groups(data, (d) => d.commit);
+
+function processCommits() {
+  commits = d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/vis-society/lab-7/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        configurable: false, //  Prevents changing the property attributes
+        writable: true, // Allows modifying the 'lines' value
+        enumerable: true // Includes 'lines' in property enumeration
+      });
+
+      return ret;
+    });
 }
 
 document.addEventListener('DOMContentLoaded', async () => {
   await loadData();
 });
+
+function displayStats() {
+  // Process commits first
+  processCommits();
+
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>:');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('Total commits:');
+  dl.append('dd').text(commits.length);
+
+  // Add more stats as needed...
+  const num_files = d3.group(data, d => d.file).size
+  dl.append('dt').html('Total Files:');
+  dl.append('dd').text(num_files);
+
+  const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' })
+  );
+  const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+  dl.append('dt').text('Most Work Done:');
+  dl.append('dd').text(maxPeriod);
+
+  const workByDay = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).getDay());
+
+  const maxDay = d3.greatest(workByDay, (d) => d[1])?.[0];
+  
+  const days_week = {0:'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday',
+    4: 'Thursday', 5: 'Friday', 6:'Saturday'};
+
+  dl.append('dt').text('Favorite Day to Work:');
+  dl.append('dd').text(days_week[maxDay]);
+
+}
+
+
+function createScatterplot() {
+  const width = 1000;
+  const height = 600;
+
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('viewBox', `0 0 ${width} ${height}`)
+    .style('overflow', 'visible');
+
+  const xScale = d3
+    .scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([0, width])
+    .nice();
+
+  const yScale = d3.scaleLinear().domain([0, 24]).range([height, 0]);
+
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+  
+  // Update scales with new ranges
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Create the axes
+const xAxis = d3.axisBottom(xScale);
+
+const yAxis = d3
+  .axisLeft(yScale)
+  .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+// Add X axis
+svg
+  .append('g')
+  .attr('transform', `translate(0, ${usableArea.bottom})`)
+  .call(xAxis);
+
+// Add Y axis
+svg
+  .append('g')
+  .attr('transform', `translate(${usableArea.left}, 0)`)
+  .call(yAxis);
+
+const dots = svg.append('g').attr('class', 'dots');
+
+dots
+  .selectAll('circle')
+  .data(commits)
+  .join('circle')
+  .attr('cx', (d) => xScale(d.datetime))
+  .attr('cy', (d) => yScale(d.hourFrac))
+  .attr('r', 5)
+  .attr('fill', 'steelblue');
+}
